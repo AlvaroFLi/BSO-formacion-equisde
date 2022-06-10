f82e5a367b2ea276def4cdea544cc40ae3d15b79
alvaroF01 <alvaro.fernandez@bosonit.com>
2022-06-10 11:25:09 +0200

    fallo xd

--- Avanzando_con_los_controladores/UsuarioRepositorio.java
+++
@@ -1,8 +0,0 @@
-package RS1_Avanzando_con_los_controladores;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UsuarioRepositorio extends JpaRepository<Usuarios,Integer>{
-}
